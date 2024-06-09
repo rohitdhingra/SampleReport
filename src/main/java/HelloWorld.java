07b8a66c0381b844b0918845fa8f2b55c98331c6
Rohit <er.rohit.dhingra@gmail.com>
2024-06-09 14:48:01 +0530

    b6 chanf

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello master");
+    System.out.println("Hello b6");
     System.out.println("Feature Branch Changes");
   }
 }
